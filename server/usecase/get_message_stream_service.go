cf254f2dc46ebf82493cad9cd727ebe9c38cbf1f
kawamou <kwmkzsd@gmail.com>
2022-07-31 23:00:29 +0900

    MessageのStreamを取得するユースケースを追加

---
+++ server/usecase/get_message_stream_service.go
@@ -0,0 +1,39 @@
+package usecase
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kawamou/grpcchat/domain"
+	"golang.org/x/sync/errgroup"
+)
+
+type GetMessageStreamService struct {
+	messageRepository domain.MessageRepository
+}
+
+func NewGetMessageStreamService(repo domain.MessageRepository) *GetMessageStreamService {
+	return &GetMessageStreamService{
+		messageRepository: repo,
+	}
+}
+
+func (g *GetMessageStreamService) Handle(ctx context.Context, stream chan<- domain.Message) error {
+	defer close(stream)
+	eg, childCtx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		select {
+		case <-childCtx.Done():
+			return childCtx.Err()
+		default:
+			if err := g.messageRepository.Listen(ctx, stream); err != nil {
+				return err
+			}
+			return nil
+		}
+	})
+	if err := eg.Wait(); err != nil {
+		return fmt.Errorf("failed to GetMessageStreamService.Handle: %s", err)
+	}
+	return nil
+}

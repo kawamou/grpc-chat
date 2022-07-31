10255f8865cfc69a5d0a6f443751ee7a84145e78
kawamou <kwmkzsd@gmail.com>
2022-07-31 23:00:44 +0900

    Messageを永続化するユースケースを追加

---
+++ server/usecase/create_message_service.go
@@ -0,0 +1,36 @@
+package usecase
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/kawamou/grpcchat/domain"
+)
+
+type CreateMessageService struct {
+	messageRepository domain.MessageRepository
+}
+
+type CreateMessageServiceInput struct {
+	message *domain.Message
+}
+
+func NewCreateMessageService(repo domain.MessageRepository) *CreateMessageService {
+	return &CreateMessageService{
+		messageRepository: repo,
+	}
+}
+
+func NewCreateMessageServiceInput(name, message string, createdAt time.Time) *CreateMessageServiceInput {
+	return &CreateMessageServiceInput{
+		message: domain.NewMessage(name, message, createdAt),
+	}
+}
+
+func (g *CreateMessageService) Handle(ctx context.Context, input *CreateMessageServiceInput) error {
+	if err := g.messageRepository.Add(ctx, input.message); err != nil {
+		return fmt.Errorf("failed to CreateMessageService.Handle: %w", err)
+	}
+	return nil
+}

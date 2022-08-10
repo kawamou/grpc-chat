3051d18299b8a90bd9c176e90bf9e72f0b40e1b9
kawamou <kwmkzsd@gmail.com>
2022-08-11 02:17:04 +0900

    Firestoreではなくメモリ上のメッセージRepositoryを注入するように変更

--- server/main.go
+++ server/main.go
@@ -52,8 +52,9 @@ func main() {
 }
 
 func register(ctx context.Context, s *grpc.Server) {
-	c := infra.NewFirestoreClient(ctx)
-	repo := infra.NewMessageRepositoryImpl(c)
+	// c := infra.NewFirestoreClient(ctx)
+	// repo := infra.NewMessageRepositoryImpl(c)
+	repo := infra.NewLocalMessageRepositoryImpl()
 	createMessageService := usecase.NewCreateMessageService(repo)
 	getMessageService := usecase.NewGetMessageStreamService(repo)
 	pb.RegisterChatServiceServer(s, NewServer(*createMessageService, *getMessageService))
